52e49e89b17bc4ea808f5b8dc8a0005424ed7472
anderash <anders.aasheim@hotmail.com>
2015-01-16 12:27:47 +0100

    Ikke fungerende, men godkjent

--- Exercise1/helloworld_python.py
+++ Exercise1/helloworld_python.py
@@ -1,25 +1,31 @@
-# Python 3.3.3 and 2.7.6
-# python helloworld_python.py
-
 from threading import Thread
-
 i = 0
 
-def someThreadFunction():
-    print("Hello from a thread!")
+
+def someThreadFunction1():
+	for j in range (0, 1000000):
+		i += 1 
+	print("Hello from a thread 1!")
+
+def someThreadFunction2():
+	for j in range (0, 1000000):
+		i -= 1 
+	print("Hello from a thread 2!")
 
 # Potentially useful thing:
-#   In Python you "import" a global variable, instead of "export"ing it when you declare it
-#   (This is probably an effort to make you feel bad about typing the word "global")
-    global i
+# In Python you "import" a global variable, instead of "export"ing it when you declare it
+# (This is probably an effort to make you feel bad about typing the word "global")
+
 
 
 def main():
-    someThread = Thread(target = someThreadFunction, args = (),)
-    someThread.start()
-    
-    someThread.join()
-    print("Hello from main!")
-
+	
+	someThread1 = Thread(target = someThreadFunction1, args = (),)
+	someThread2 = Thread(target = someThreadFunction2, args = (),)
+	someThread1.start()
+	someThread2.start()
+	someThread1.join()
+	someThread2.join()
+	print(i, "Hello from main!")
 
 main()
