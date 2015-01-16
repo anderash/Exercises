6dcc4e9df7cec0ad1d440a4062177001d5442b15
marty78 <martinhals@outlook.com>
2015-01-16 12:29:22 +0100

    Ville ikke printe, men godkjent

--- Exercise1/thread.c
+++ Exercise1/thread.c
@@ -1,22 +1,32 @@
 #include <pthread.h>
-#include <studio.h>
+#include <stdio.h>
 
+int i = 0;
 
-void* thread1(int i){
-	while(i <= 1000000){
+void* threadFunc1(){
+	for(int j = 0; j<=1000000; j+=1){
 		i += 1;
 	}
+	return 0;
 }
 
-void* thread2(int i){
-	while(i >= -1000000){
+void* threadFunc2(){
+	for(int j = 0; j<=1000000; j+=1){
 		i -= 1;
 	}
+	return 0;
 }
 
 int main(){
-	i = 0;
+	pthread_t someThread1;
+	pthread_t someThread2;
+    pthread_create(&someThread1, NULL, threadFunc1, NULL);
+    pthread_create(&someThread2, NULL, threadFunc2, NULL);
 
+    pthread_join(someThread1, NULL);
 
+    printf("%d\n", i);
+
+    return 0;
 
 }
