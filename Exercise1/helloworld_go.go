554341de454a48b18f8b6122e8ad9a6600682f6c
anderash <anders.aasheim@hotmail.com>
2015-01-16 12:28:24 +0100

    Fungerer og godkjent. Skriver random

--- Exercise1/helloworld_go.go
+++ Exercise1/helloworld_go.go
@@ -1,34 +1,32 @@
-// Go 1.2
-// go run helloworld_go.go
-
 package main
-
 import (
-    . "fmt"     // Using '.' to avoid prefixing functions with their package names
-                //   This is probably not a good idea for large projects...
-    "runtime"
-    "time"
+	. "fmt" // Using '.' to avoid prefixing functions with their package names
+// This is probably not a good idea for large projects...
+	"runtime"
+	"time"
 )
 
+var i int = 0
+
 func someGoroutine1() {
-    for (j = 0; j<1000000; j++) {
-        i++
-    }
+	for j := 0; j<1000000; j++ {
+		i++
+	}
 }
 func someGoroutine2() {
-    for (j = 0; j<1000000; j++) {
-        i--
+	for j := 0; j<1000000; j++ {
+		i--
+	}
 }
-
-
-
 func main() {
-    runtime.GOMAXPROCS(runtime.NumCPU())    // I guess this is a hint to what GOMAXPROCS does...
-                                            // Try doing the exercise both with and without it!
-    go someGoroutine1()                      // This spawns someGoroutine() as a goroutine
-    go someGoroutine2()
-    // We have no way to wait for the completion of a goroutine (without additional syncronization of some sort)
-    // We'll come back to using channels in Exercise 2. For now: Sleep.
-    time.Sleep(100*time.Millisecond)
-    Println("Hello from main!")
+	
+	runtime.GOMAXPROCS(runtime.NumCPU()) // I guess this is a hint to what GOMAXPROCS does...
+										// Try doing the exercise both with and without it!
+	go someGoroutine1() // This spawns someGoroutine() as a goroutine
+	go someGoroutine2()
+// We have no way to wait for the completion of a goroutine (without additional syncronization of some sort)
+// We'll come back to using channels in Exercise 2. For now: Sleep.
+	time.Sleep(100*time.Millisecond)
+	Println(i)
+	Println("Hello from main!")
 }
