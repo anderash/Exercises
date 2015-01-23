438c6745ac40171405fdcf37b9073c59cea6f5be
anderash <anders.aasheim@hotmail.com>
2015-01-23 12:13:41 +0100

    Handle err

--- Exercise3/UDP.go
+++ Exercise3/UDP.go
@@ -8,6 +8,10 @@ import (
 func main() {
  // var buffer byte = 0 
   conn, err := net.Dial("udp", ":30000")
+  if err != nil {
+    fmt.Printf("Noe gikk til helvette")
+  }
+  
   addr := conn.LocalAddr()
   fmt.Printf(addr.String())
 }
