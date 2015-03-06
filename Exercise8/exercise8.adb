51cfe52b173d9c5b9de2353812199e3c76471a17
Anders Åsheim <anders.aasheim@hotmail.com>
2015-03-06 11:43:51 +0100

    Fungerende Ex8

---
+++ Exercise8/exercise8.adb
@@ -0,0 +1,146 @@
+with Ada.Text_IO, Ada.Integer_Text_IO, Ada.Numerics.Float_Random;
+use  Ada.Text_IO, Ada.Integer_Text_IO, Ada.Numerics.Float_Random;
+
+procedure exercise8 is
+
+    Count_Failed    : exception;    -- Exception to be raised when counting fails
+    Gen             : Generator;    -- Random number generator
+
+    protected type Transaction_Manager (N : Positive) is
+        entry Finished;
+        entry wait_until_aborted;
+        function Commit return Boolean;
+        procedure Signal_Abort;
+    private
+        Finished_Gate_Open  : Boolean := False;
+        Aborted             : Boolean := False;
+        Should_Commit       : Boolean := True;
+    end Transaction_Manager;
+    protected body Transaction_Manager is
+        entry Finished when Finished_Gate_Open or Finished'Count = N is
+        begin
+            ------------------------------------------
+            -- PART 3: Complete the exit protocol here
+            ------------------------------------------
+            if Finished'Count = N-1 then
+                Finished_Gate_Open := True;
+            end if;
+            if Finished'Count = 0 then
+                Finished_Gate_Open := False;
+            end if;
+            -- if Aborted = True then
+            --     Should_Commit := False;
+            -- end if;
+
+        end Finished;
+
+        entry wait_until_aborted when Aborted is
+        begin
+
+            if wait_until_aborted'Count = 0 then                
+                Aborted := False;
+            end if;    
+
+        end wait_until_aborted;
+
+        procedure Signal_Abort is
+        begin
+            Aborted := True;
+        end Signal_Abort;
+
+        function Commit return Boolean is
+        begin
+            return Should_Commit;
+        end Commit;
+        
+    end Transaction_Manager;
+
+
+
+    
+    function Unreliable_Slow_Add (x : Integer) return Integer is
+    Error_Rate : Constant := 0.15;  -- (between 0 and 1)
+    Random_Number : Float;
+    Mult_Number : Float;
+    begin
+        -------------------------------------------
+        -- PART 1: Create the transaction work here
+        -------------------------------------------
+        Random_number := Random(Gen);
+
+        if Random_number > Error_Rate then
+            Mult_Number := Random_number*10.0;
+            delay Duration(Mult_Number);
+            Put_Line("Added 10 to number!");
+            return x + 10;
+        else
+            delay Duration(0.5);
+            raise Count_Failed;
+            return x;
+        end if;
+
+    end Unreliable_Slow_Add;
+
+
+
+
+    task type Transaction_Worker (Initial : Integer; Manager : access Transaction_Manager);
+    task body Transaction_Worker is
+        Num         : Integer   := Initial;
+        Prev        : Integer   := Num;
+        Round_Num   : Integer   := 0;
+    begin
+
+        Put_Line ("Worker" & Integer'Image(Initial) & " started");
+
+        loop
+            Put_Line ("Worker" & Integer'Image(Initial) & " started round" & Integer'Image(Round_Num));
+            Round_Num := Round_Num + 1;
+
+            ---------------------------------------
+            -- PART 2: Do the transaction work here             
+            ---------------------------------------
+            begin
+                select
+                    Manager.wait_until_aborted;
+                    Num := Num + 5;
+                then abort
+                    begin
+                        Num := Unreliable_Slow_Add(Num);
+                        Manager.Finished;
+                    
+                    exception
+                        when Count_Failed =>
+                            Manager.Signal_Abort;
+                    end;
+                end select;
+
+            --     Num := Unreliable_Slow_Add(Num);
+            -- exception
+            --     when Count_Failed =>
+            --         Manager.Signal_Abort;
+            end;
+
+            
+
+            Put_Line ("  Worker" & Integer'Image(Initial) & " comitting" & Integer'Image(Num));
+
+
+
+            delay 0.5;
+
+        end loop;
+    end Transaction_Worker;
+
+    Manager : aliased Transaction_Manager (3);
+
+    Worker_1 : Transaction_Worker (0, Manager'Access);
+    Worker_2 : Transaction_Worker (1, Manager'Access);
+    Worker_3 : Transaction_Worker (2, Manager'Access);
+
+begin
+    Reset(Gen); -- Seed the random number generator
+end exercise8;
+
+
+
